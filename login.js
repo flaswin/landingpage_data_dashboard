8bdbb9aef7fdf33abefef4b47a27007d14a95634
tech@fl <flaswin34@gmail.com>
2026-02-18 20:41:25 +0530

    Update login.js

--- login.js
+++ login.js
@@ -1,7 +1,4 @@
-const supabase = window.supabase.createClient(
-"https://mficuhiqstpdwdwaayzg.supabase.co",
-"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1maWN1aGlxc3RwZHdkd2FheXpnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA4MzkwNzQsImV4cCI6MjA4NjQxNTA3NH0.WZE3Hd7XQquXuF_WaPtTLz1KFsXpfaCq-eZypbbn6pU"
-);
+const supabase = window.supabaseClient;
 
 document.getElementById("loginBtn")
 .addEventListener("click", async ()=>{
@@ -9,7 +6,7 @@ document.getElementById("loginBtn")
 const email = document.getElementById("email").value;
 const password = document.getElementById("password").value;
 
-const { data, error } = await supabase.auth.signInWithPassword({
+const { error } = await supabase.auth.signInWithPassword({
 email: email,
 password: password
 });
