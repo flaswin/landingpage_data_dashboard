56b2ff1e4ce438199366ab1297b7943d27c5ae23
tech@fl <flaswin34@gmail.com>
2026-02-21 20:10:30 +0530

    Delete supabase.js

--- supabase.js
+++
@@ -1,4 +0,0 @@
-window.supabaseClient = window.supabase.createClient(
-"https://mficuhiqstpdwdwaayzg.supabase.co",
-"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1maWN1aGlxc3RwZHdkd2FheXpnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA4MzkwNzQsImV4cCI6MjA4NjQxNTA3NH0.WZE3Hd7XQquXuF_WaPtTLz1KFsXpfaCq-eZypbbn6pU"
-);
